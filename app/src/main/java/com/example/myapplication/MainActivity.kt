b2ae059d38b456fd04f3a23e86f097e610c75e2c
zmalarconMELI <zully.alarcon@mercadolibre.com.co>
2022-11-03 11:11:53 -0500

    Add new config

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
+import android.webkit.JavascriptInterface
 import android.webkit.WebSettings
 import androidx.appcompat.app.AppCompatActivity
 import android.webkit.WebView
@@ -22,10 +24,44 @@ class MainActivity : AppCompatActivity() {
 
     fun setwebView() {
         webView = findViewById(R.id.webview)
-        webView.loadUrl("https://auth.mercadopago.com.ar/stage/authorization?client_id=7797417756487296&response_type=code&platform_id=mp&scopes=read,account.debit")
-        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://auth.mercadopago.com.ar/stage/authorization?client_id=7797417756487296&response_type=code&scopes=read,account.debit&code_challenge=OYTJTHLI_DVE45e54_HxLbhHXDvYKe3eMpbkGGs6p9E&code_challenge_method=S256")
         val settings:WebSettings = webView.settings
         settings.javaScriptEnabled = true
+        webView.webViewClient = object: WebViewClient(){
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                injectJS(view)
+            }
+        }
+        webView.addJavascriptInterface(JSBridge, "Bridge")
 
+    }
+
+    override fun onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun injectJS(view: WebView?) {
+        view!!.loadUrl(
+            """
+                javascript:(function(){
+                    let button = document.getElementById("action-accept");
+                    button.addEventListener("click", function(){
+                        Bridge.calledFromJS(window.location.href);
+                    })
+                })()
+            """.trimIndent()
+        )
+    }
+
+    object JSBridge{
+        @JavascriptInterface
+        fun calledFromJS(msg: String){
+            Log.d("DEBIN Flow", msg)
+        }
     }
 }
